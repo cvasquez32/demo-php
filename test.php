3e980fda5e2ef2a03ad951c5b62f80ae3fc1a843
carlos vasquez <cvasquez0100@gmail.com>
2025-04-07 19:45:32 -0500

    keep adding php fundamentals

--- test.php
+++ test.php
@@ -1,5 +1,37 @@
 <?php
 
+$list = [];
+$x = 5; // global scope
+$y = 4;
 $color = 'Red';
+
+function myTest() {
+  static $m = 0;
+  $name = "Carlos"; // local scope
+  global $x, $y;
+
+  echo $m;
+  echo "<br>";
+  $m++;
+  echo "<br>";
+  echo $y = $x + $y;
+  echo "<p> Hello $name from my test function </p>";
+}
+
+myTest();
+
 echo "My car is " . $color . "<br>";
+echo "number $x <br>"; // double quotes
+echo 'number ' . $x . '<br>'; // single quotes
+echo $x + $y;
+echo "<br>";
+var_dump($y);
+
+array_push($list, "carlos", "firulais");
+echo "<br>";
+print_r($list);
+
+myTest();
+
+
 ?>
